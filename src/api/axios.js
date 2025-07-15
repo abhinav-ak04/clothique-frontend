d525dfd7311c4ca987918b1ef7bb4dfe5d493925
abhinav-ak04 <abhinavk.kumar04@gmail.com>
2025-07-16 02:23:29 +0530

    fixed axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL:
     import.meta.env.VITE_BACKEND_API_BASE_URL || 'http://localhost:8000/api',
-  withCredentials: true, // useful later for cookies/auth
+  // Removed withCredentials: true since we’re not using cookies for now
 });
 
 export default axiosInstance;
